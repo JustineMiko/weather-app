4ad0cd729024d8654519522e036a8cf4121b101d
JustineMiko <justinemarzec@gmail.com>
2021-01-26 17:49:02 +0100

    ...

--- components/AllDaysWeather.js
+++ components/AllDaysWeather.js
@@ -6,7 +6,7 @@ const AllDaysWeather = ({weather}) => {
     
     // Jour 1 : 
     const dayOne = weather.daily[0];
-    const dayOnedate = dayOne.dt;
+    const dayOneDate = dayOne.dt;
     const dayOneTemp = dayOne.temp.day;
     const dayOneFeels = dayOne.feels_like.day;
     const dayOneWeatherType = dayOne.weather[0].description;
@@ -15,7 +15,7 @@ const AllDaysWeather = ({weather}) => {
     // Jour 2:
 
     const dayTwo = weather.daily[1];
-    const dayTwodate = dayTwo.dt;
+    const dayTwoDate = dayTwo.dt;
     const dayTwoTemp = dayTwo.temp.day;
     const dayTwoFeels = dayTwo.feels_like.day;
     const dayTwoWeatherType = dayTwo.weather[0].description;
@@ -24,7 +24,7 @@ const AllDaysWeather = ({weather}) => {
     // Jour 3 :
 
     const dayThree = weather.daily[2];
-    const dayThreedate = dayThree.dt;
+    const dayThreeDate = dayThree.dt;
     const dayThreeTemp = dayThree.temp.day;
     const dayThreeFeels = dayThree.feels_like.day;
     const dayThreeWeatherType = dayThree.weather[0].description;
@@ -33,7 +33,7 @@ const AllDaysWeather = ({weather}) => {
     // Jour 4 :
 
     const dayFour = weather.daily[3];
-    const dayFourdate = dayFour.dt;
+    const dayFourDate = dayFour.dt;
     const dayFourTemp = dayFour.temp.day;
     const dayFourFeels = dayFour.feels_like.day;
     const dayFourWeatherType = dayFour.weather[0].description;
@@ -42,7 +42,7 @@ const AllDaysWeather = ({weather}) => {
      // Jour 5 :
 
     const dayFive = weather.daily[4];
-    const dayFivedate = dayFive.dt;
+    const dayFiveDate = dayFive.dt;
     const dayFiveTemp = dayFive.temp.day;
     const dayFiveFeels = dayFive.feels_like.day;
     const dayFiveWeatherType = dayFive.weather[0].description;
@@ -51,7 +51,7 @@ const AllDaysWeather = ({weather}) => {
     // Jour 6 :
 
     const daySix = weather.daily[5];
-    const daySixdate = daySix.dt;
+    const daySixDate = daySix.dt;
     const daySixTemp = daySix.temp.day;
     const daySixFeels = daySix.feels_like.day;
     const daySixWeatherType = daySix.weather[0].description;
@@ -60,7 +60,7 @@ const AllDaysWeather = ({weather}) => {
     // Jour 7 :
 
     const daySeven = weather.daily[6];
-    const daySevendate = daySeven.dt;
+    const daySevenDate = daySeven.dt;
     const daySevenTemp = daySeven.temp.day;
     const daySevenFeels = daySeven.feels_like.day;
     const daySevenWeatherType = daySeven.weather[0].description;
@@ -71,44 +71,51 @@ const AllDaysWeather = ({weather}) => {
         <ScrollView>
         <SafeAreaView style={styles.weekSection}>
         <Image>{dayOneIcon}</Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayOnedate}</Text>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayOneDate}</Text>
         <Text style={styles.weekText}>16° / {dayOneTemp}</Text>
         <Text style={styles.weekText}>Rain / {dayOneWeatherType}</Text>
         </SafeAreaView>
        
         <SafeAreaView style={styles.weekSection}>
-        <Image></Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021</Text>
-        <Text style={styles.weekText}>16°</Text>
+        <Image>{dayTwoIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayTwoDate}</Text>
+        <Text style={styles.weekText}>16° / {dayTwoTemp}</Text>
         <Text style={styles.weekText}>Rain</Text>
         </SafeAreaView>
         
         <SafeAreaView style={styles.weekSection}>
-        <Image></Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021</Text>
-        <Text style={styles.weekText}>16°</Text>
-        <Text style={styles.weekText}>Rain</Text>
+        <Image>{dayThreeIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayTwoDate}</Text>
+        <Text style={styles.weekText}>16°/ {dayTwoTemp}</Text>
+        <Text style={styles.weekText}>Rain / {dayTwoWeatherType}</Text>
         </SafeAreaView>
 
         <SafeAreaView style={styles.weekSection}>
-        <Image></Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021</Text>
-        <Text style={styles.weekText}>16°</Text>
-        <Text style={styles.weekText}>Rain</Text>
+        <Image>{dayFourIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayForDate}</Text>
+        <Text style={styles.weekText}>16° / {dayFourTemp}</Text>
+        <Text style={styles.weekText}>Rain / {dayFourWeatherType}</Text>
         </SafeAreaView>
 
         <SafeAreaView style={styles.weekSection}>
-        <Image></Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021</Text>
-        <Text style={styles.weekText}>16°</Text>
-        <Text style={styles.weekText}>Rain</Text>
+        <Image>{dayFiveIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {dayFiveDate}</Text>
+        <Text style={styles.weekText}>16° / {dayFiveTemp}</Text>
+        <Text style={styles.weekText}>Rain / {dayFiveWeatherType}</Text>
         </SafeAreaView>
 
         <SafeAreaView style={styles.weekSection}>
-        <Image></Image>
-        <Text style={styles.weekText}>Tuesday January 25 2021</Text>
-        <Text style={styles.weekText}>16°</Text>
-        <Text style={styles.weekText}>Rain</Text>
+        <Image>{daySixIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {daySixDate}</Text>
+        <Text style={styles.weekText}>16° / {daySixTemp}</Text>
+        <Text style={styles.weekText}>Rain / {daySixWeatherType}</Text>
+        </SafeAreaView>
+
+        <SafeAreaView style={styles.weekSection}>
+        <Image>{daySevenIcon}</Image>
+        <Text style={styles.weekText}>Tuesday January 25 2021 / {daySevenDate}</Text>
+        <Text style={styles.weekText}>16°/ {daySevenTemp}</Text>
+        <Text style={styles.weekText}>Rain / {daySevenWeatherType}</Text>
         </SafeAreaView>
 
         </ScrollView>
