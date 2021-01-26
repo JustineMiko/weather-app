45e719edb34c1d44874e467f2190482f377c6903
JustineMiko <justinemarzec@gmail.com>
2021-01-26 12:18:03 +0100

    App.js

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
+import AllDaysWeather from './components/AllDaysWeather';
+import axios from 'axios';
+import DayWeather from './components/DayWeather';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+
+  const [Weather, setWeather] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://api.openweathermap.org/data/2.5/onecall?lat=50.633333&lon=3.066667&exclude=minutely,hourly,alerts&&appid=6e2c8f258ad2ee898b5f95e8ee374d29')
+    .then(res => {
+      setWeather(res.data);
+    })
+  }, []);
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        <DayWeather />
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+
   },
+
+  title: {
+    color: 'white',
+  }
+
 });
